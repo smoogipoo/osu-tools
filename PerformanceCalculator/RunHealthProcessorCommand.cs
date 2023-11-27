57eaab02a6d775fe3f55cf2215648c5ac275c962
Dan Balasescu <smoogipoo@smgi.me>
2023-11-27 14:46:19 +0900

    Add case for mania ruleset

--- PerformanceCalculator/RunHealthProcessorCommand.cs
+++ PerformanceCalculator/RunHealthProcessorCommand.cs
@@ -14,6 +14,8 @@ using osu.Game.Rulesets;
 using osu.Game.Rulesets.Catch.Objects;
 using osu.Game.Rulesets.Catch.Scoring;
 using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.Mania.Objects;
+using osu.Game.Rulesets.Mania.Scoring;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.Osu.Mods;
@@ -97,6 +99,21 @@ namespace PerformanceCalculator
                     };
 
                     newProcessor = new CatchHealthProcessor(playableBeatmap.HitObjects[0].StartTime)
+                    {
+                        OnIterationFail = onIterationFail,
+                        OnIterationSuccess = onIterationSuccess
+                    };
+
+                    break;
+
+                case 3:
+                    legacyProcessor = new LegacyManiaHealthProcessor(playableBeatmap.HitObjects[0].StartTime)
+                    {
+                        OnIterationFail = onIterationFail,
+                        OnIterationSuccess = onIterationSuccess,
+                    };
+
+                    newProcessor = new ManiaHealthProcessor(playableBeatmap.HitObjects[0].StartTime)
                     {
                         OnIterationFail = onIterationFail,
                         OnIterationSuccess = onIterationSuccess
@@ -559,6 +576,172 @@ namespace PerformanceCalculator
 
                         yield return (hitObject, newCombo);
                     }
+                }
+            }
+        }
+
+        #endregion
+
+        #region mania
+
+        /// <summary>
+        /// Reference implementation for osu!stable's HP drain.
+        /// Cannot be used for gameplay.
+        /// </summary>
+        public partial class LegacyManiaHealthProcessor : DrainingHealthProcessor
+        {
+            private const double hp_bar_maximum = 200;
+
+            public Action<string> OnIterationFail;
+            public Action<string> OnIterationSuccess;
+
+            private double lowestHpEver;
+            private double lowestHpEnd;
+            private double hpRecoveryAvailable;
+            private double hpMultiplierNormal;
+
+            public LegacyManiaHealthProcessor(double drainStartTime)
+                : base(drainStartTime)
+            {
+            }
+
+            public override void ApplyBeatmap(IBeatmap beatmap)
+            {
+                lowestHpEver = IBeatmapDifficultyInfo.DifficultyRange(beatmap.Difficulty.DrainRate, 195, 160, 60);
+                lowestHpEnd = IBeatmapDifficultyInfo.DifficultyRange(beatmap.Difficulty.DrainRate, 198, 180, 80);
+                hpRecoveryAvailable = IBeatmapDifficultyInfo.DifficultyRange(beatmap.Difficulty.DrainRate, 8, 4, 0);
+
+                base.ApplyBeatmap(beatmap);
+            }
+
+            protected override void ApplyResultInternal(JudgementResult result)
+            {
+                if (!IsSimulating)
+                    throw new NotSupportedException("The legacy osu! health processor is not supported for gameplay.");
+            }
+
+            protected override void RevertResultInternal(JudgementResult result)
+            {
+                if (!IsSimulating)
+                    throw new NotSupportedException("The legacy osu! health processor is not supported for gameplay.");
+            }
+
+            protected override void Reset(bool storeResults)
+            {
+                hpMultiplierNormal = 1;
+
+                base.Reset(storeResults);
+            }
+
+            protected override double ComputeDrainRate()
+            {
+                double testDrop = 0.05;
+                double currentHp;
+                double currentHpUncapped;
+
+                do
+                {
+                    currentHp = hp_bar_maximum;
+                    currentHpUncapped = hp_bar_maximum;
+
+                    double lowestHp = currentHp;
+                    double lastTime = DrainStartTime;
+                    int currentBreak = 0;
+                    bool fail = false;
+                    string failReason = string.Empty;
+
+                    for (int i = 0; i < Beatmap.HitObjects.Count; i++)
+                    {
+                        HitObject h = Beatmap.HitObjects[i];
+
+                        // Find active break (between current and lastTime)
+                        double localLastTime = lastTime;
+                        double breakTime = 0;
+
+                        // Subtract any break time from the duration since the last object
+                        if (Beatmap.Breaks.Count > 0 && currentBreak < Beatmap.Breaks.Count)
+                        {
+                            BreakPeriod e = Beatmap.Breaks[currentBreak];
+
+                            if (e.StartTime >= localLastTime && e.EndTime <= h.StartTime)
+                            {
+                                // consider break start equal to object end time for version 8+ since drain stops during this time
+                                breakTime = (Beatmap.BeatmapInfo.BeatmapVersion < 8) ? (e.EndTime - e.StartTime) : e.EndTime - localLastTime;
+                                currentBreak++;
+                            }
+                        }
+
+                        reduceHp(testDrop * (h.StartTime - lastTime - breakTime));
+
+                        lastTime = h.GetEndTime();
+
+                        if (currentHp < lowestHp)
+                            lowestHp = currentHp;
+
+                        if (currentHp <= lowestHpEver)
+                        {
+                            fail = true;
+                            testDrop *= 0.96;
+                            failReason = $"hp too low ({currentHp / hp_bar_maximum} < {lowestHpEver / hp_bar_maximum})";
+                            break;
+                        }
+
+                        double hpReduction = testDrop * (h.GetEndTime() - h.StartTime);
+                        double hpOverkill = Math.Max(0, hpReduction - currentHp);
+                        reduceHp(hpReduction);
+
+                        if (h is HoldNote)
+                            increaseHp(hpMultiplierNormal * (1.1 - Beatmap.Difficulty.DrainRate * 0.1));
+
+                        if (hpOverkill > 0 && currentHp - hpOverkill <= lowestHpEver)
+                        {
+                            fail = true;
+                            testDrop *= 0.96;
+                            failReason = $"overkill ({currentHp / hp_bar_maximum} - {hpOverkill / hp_bar_maximum} <= {lowestHpEver / hp_bar_maximum})";
+                            break;
+                        }
+
+                        increaseHp(hpMultiplierNormal * (1.1 - Beatmap.Difficulty.DrainRate * 0.1));
+                    }
+
+                    if (!fail && currentHp < lowestHpEnd)
+                    {
+                        fail = true;
+                        testDrop *= 0.94;
+                        hpMultiplierNormal *= 1.01;
+                        failReason = $"end hp too low ({currentHp / hp_bar_maximum} < {lowestHpEnd / hp_bar_maximum})";
+                    }
+
+                    double recovery = (currentHpUncapped - hp_bar_maximum) / Beatmap.HitObjects.Count;
+
+                    if (!fail && recovery < hpRecoveryAvailable)
+                    {
+                        fail = true;
+                        testDrop *= 0.96;
+                        hpMultiplierNormal *= 1.01;
+                        failReason = $"recovery too low ({recovery / hp_bar_maximum} < {hpRecoveryAvailable / hp_bar_maximum})";
+                    }
+
+                    if (fail)
+                    {
+                        OnIterationFail?.Invoke($"FAILED drop {testDrop / hp_bar_maximum}: {failReason}");
+                        continue;
+                    }
+
+                    OnIterationSuccess?.Invoke($"PASSED drop {testDrop / hp_bar_maximum}");
+                    return testDrop / hp_bar_maximum;
+                } while (true);
+
+                void reduceHp(double amount)
+                {
+                    currentHpUncapped = Math.Max(0, currentHpUncapped - amount);
+                    currentHp = Math.Max(0, currentHp - amount);
+                }
+
+                void increaseHp(double amount)
+                {
+                    currentHpUncapped += amount;
+                    currentHp = Math.Max(0, Math.Min(hp_bar_maximum, currentHp + amount));
                 }
             }
         }
